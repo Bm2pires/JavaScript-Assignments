6fe159ee9590dac4cd048eb2ecec1282523fe1c9
Bredan Pires <piresbredan@gmail.com>
2021-11-18 16:05:26 +0000

    completed exercise 7

---
+++ 7-highlight-text-on-mouseover.js
@@ -0,0 +1,7 @@
+function highlightText() {
+  document.getElementById("boldText").style.backgroundColor = "yellow";
+}
+
+function unHighlightText() {
+  document.getElementById("boldText").style.backgroundColor = "transparent";
+}
