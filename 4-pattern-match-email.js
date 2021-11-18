3230896ceae56a3bbea8409c9289e0b5a4e60436
Bredan Pires <piresbredan@gmail.com>
2021-11-18 14:51:40 +0000

    completed exercise 4

--- 4-pattern-match-email.js
+++ 4-pattern-match-email.js
@@ -1,4 +1,12 @@
 // write a pattern that matches e-mail addresses
-var email = "piresbredan@gmail.com";
+var email = "piresbredangmail.com";
 const emailRegExp =
   /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+let validateEmail = emailRegExp.test(email);
+
+if (validateEmail == true) {
+  console.log(`${email} is a valid email.`);
+} else {
+  console.log(`${email} is not a valid email`);
+}
